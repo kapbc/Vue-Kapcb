4f1daf28315e54fe6b3a52903916902b5e6916c6
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-08-28 23:57:55 +0800

    export

--- ES6exportAndimport/export.js
+++ ES6exportAndimport/export.js
@@ -6,8 +6,17 @@ function sum(num1, num2) {
   return num1 + num2;
 }
 
+/**
+ * 1.导出方式一
+ */
 export {
   name,
   age,
   flag
 }
+
+/**
+ * 2.导出方式二
+ */
+export let email = 'eircccallroot@yeah.net';
+export let address = '深圳';
--- ES6exportAndimport/import.js
+++ ES6exportAndimport/import.js
@@ -1,8 +1,13 @@
 import {flag} from "./export.js";
 import {name} from "./export.js";
 import {age} from "./export.js";
+import {email} from "./export.js";
+import {address} from "./export.js";
 
 if (flag) {
   console.log('Kapcb nb!');
 }
+
 console.log('My name is : ' + name + ' age is : ' + age);
+
+console.log('My email is : ' + email + ' address is : ' + address);
