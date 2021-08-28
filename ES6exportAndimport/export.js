90d79bbb7de09301a31c04b7f35acd967003bc4d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-08-29 00:16:59 +0800

    export-default

--- ES6exportAndimport/export.js
+++ ES6exportAndimport/export.js
@@ -7,7 +7,7 @@ function sum(num1, num2) {
 }
 
 /**
- * 1.导出方式一
+ * 1.导出变量方式一
  */
 export {
   name,
@@ -16,7 +16,44 @@ export {
 }
 
 /**
- * 2.导出方式二
+ * 2.导出变量方式二
  */
 export let email = 'eircccallroot@yeah.net';
 export let address = '深圳';
+
+/**
+ * 3.导出函数方式一
+ */
+export function logMessage(message) {
+  console.log('这是export.js中的导出函数');
+  console.log(message);
+}
+
+/**
+ * 4.导出整个类
+ */
+export class Person {
+  // name;
+  // age;
+
+  /**
+   * ES6中定义构造器
+   * @param name String
+   * @param age int
+   */
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  run() {
+    console.log('这是Person类中的run方法');
+    console.log('My name is : ' + this.name + ' age is : ' + this.age);
+  }
+}
+
+/**
+ * 某些情况下 ,一个模块中包含某个功能 ,但是开发者并不希望给这个功能命名, 而是让导入者自己来命名
+ * 这种情况就使用 export default
+ */
+// export default
--- ES6exportAndimport/import.js
+++ ES6exportAndimport/import.js
@@ -1,8 +1,22 @@
+/**
+ * 导入变量方式一
+ * 在node中可以省略 "./export.js" 中的.js
+ */
 import {flag} from "./export.js";
 import {name} from "./export.js";
 import {age} from "./export.js";
-import {email} from "./export.js";
-import {address} from "./export.js";
+/**
+ * 导入变量方式二
+ */
+import {email, address} from "./export.js";
+/**
+ * 导入方法
+ */
+import {logMessage} from "./export.js";
+/**
+ * 导入整个类
+ */
+import {Person} from "./export.js";
 
 if (flag) {
   console.log('Kapcb nb!');
@@ -11,3 +25,10 @@ if (flag) {
 console.log('My name is : ' + name + ' age is : ' + age);
 
 console.log('My email is : ' + email + ' address is : ' + address);
+
+logMessage('这是来自import.js中的消息');
+
+let person = new Person();
+person.name = 'Kapcb';
+person.age = 24;
+person.run();
