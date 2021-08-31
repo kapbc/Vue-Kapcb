e7969f6e5018d87b8f5c2efa62b7aebc1c040b11
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-08-31 22:11:42 +0800

    webpack.config.js

--- webpack-config/webpack.config.js
+++ webpack-config/webpack.config.js
@@ -1,9 +1,12 @@
+// 从node中动态获取文件路径
+const path = require('path');
+
 module.exports = {
   // 入口
   entry: './src/main.js',
   // 出口
   output: {
-    // 路径
+    // 路径(动态获取文件路径)
     path: './dist',
     // 文件名
     filename: 'bundle.js',
