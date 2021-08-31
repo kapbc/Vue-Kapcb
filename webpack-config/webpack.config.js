9198d3b80aa6428f9bf28ab0d6753d798a73628c
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-08-31 22:40:48 +0800

    config

--- webpack-config/webpack.config.js
+++ webpack-config/webpack.config.js
@@ -2,11 +2,11 @@
 const path = require('path');
 
 module.exports = {
-  // 入口
+  // 入口: 可以是字符串、数组、对象
   entry: './src/main.js',
-  // 出口
+  // 出口: 通常是一个对象, 里面至少包含两个重要的属性, path和filename
   output: {
-    // 路径(动态获取文件路径)
+    // 绝对路径(动态获取文件路径)
     path: path.resolve(__dirname, 'dist'),
     // 文件名
     filename: 'bundle.js',
