24f90b0b25c5eee5bafa56cb8ebf87d5aacc966b
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-01 23:12:50 +0800

    command

--- webpack-loader/webpack.config.js
+++ webpack-loader/webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: {
+      // 匹配css文件, $表示结尾
+      test: /\.css$/,
+      // style-loader负责将样式添加到DOM中
+      // css-loader负责加载css文件
+      // 按道理应该先将css文件进行加载, 然后再将style样式添加到DOM中,这里之所以先配置style-loader在配置css-loader
+      // 是因为webpack在使用多个loader时, 是从右向左读取配置的loader
+      use: ['style-loader', 'css-loader']
+    }
   }
 }
 
