94481b047e3cfe16e3fea7b5acbb76c2c3650210
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-05 00:40:21 +0800

    url

--- webpack-resources/src/js/main.js
+++ webpack-resources/src/js/main.js
@@ -1,6 +1,6 @@
 require('../css/normal.css');
 require('../css/special.less');
-const Info = require('../js/info');
+let Info = require('../js/info');
 
-const personInfo = new Info.PersonInfo('kapcb', 17, '男', '深圳');
+let personInfo = new Info.PersonInfo('kapcb', 17, '男', '深圳');
 Info.HighQualityPersonCheck(personInfo);
