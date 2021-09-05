c177cb0f026da026f174510bfaeee2a43f28062d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-05 18:46:56 +0800

    vue

--- webpack-vue/src/js/main.js
+++ webpack-vue/src/js/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue';
 
 const demo = new Vue({
   el: '#demo',
+  // 这里定义的template在编译的时候会被vue复制到在html页面中挂载的元素下
+  // 即会被复制到index.html中的<div id="demo"></div>里面
+  template: `
+    <div>
+    <h2>这是main.js中的template</h2>
+    {{ message }}
+    <button @click="btnClick">按钮</button>
+    </div>
+  `,
   data: {
     message: 'Hello Webpack!'
+  },
+  methods: {
+    btnClick() {
+      alert('button click!');
+    }
   }
 });
