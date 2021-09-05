e25623ba01d9cbac0066799b67072194f30a722d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-05 21:53:09 +0800

    component

--- webpack-vue/src/js/main.js
+++ webpack-vue/src/js/main.js
@@ -1,22 +1,50 @@
 import Vue from 'vue';
 
-const demo = new Vue({
-  el: '#demo',
-  // 这里定义的template在编译的时候会被vue复制到在html页面中挂载的元素下
-  // 即会被复制到index.html中的<div id="demo"></div>里面
+// const demo = new Vue({
+//   el: '#demo',
+//   // 这里定义的template在编译的时候会被vue复制到在html页面中挂载的元素下
+//   // 即会被复制到index.html中的<div id="demo"></div>里面
+//   template: `
+//     <div>
+//     <h2>这是main.js中的template</h2>
+//     {{ message }}
+//     <button @click="btnClick">按钮</button>
+//     </div>
+//   `,
+//   data: {
+//     message: 'Hello Webpack!'
+//   },
+//   methods: {
+//     btnClick() {
+//       alert('button click!');
+//     }
+//   }
+// });
+
+const demoComponent = {
   template: `
     <div>
-    <h2>这是main.js中的template</h2>
+    <h2>这是main.js中的demoComponent中的template</h2>
     {{ message }}
     <button @click="btnClick">按钮</button>
     </div>
   `,
-  data: {
-    message: 'Hello Webpack!'
+  data() {
+    return {
+      message: 'Hello Webpack!',
+    }
   },
   methods: {
     btnClick() {
       alert('button click!');
     }
+  },
+}
+
+new Vue({
+  el: '#demo',
+  template: '<demoComponent/>',
+  components: {
+    demoComponent,
   }
-});
+})
