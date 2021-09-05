f394a729935799edfc6412338cfbeb2da6be8177
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-05 18:29:28 +0800

    vue

--- webpack-vue/webpack.config.js
+++ webpack-vue/webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '../dist/'
   },
+  // 配置vue
+  resolve: {
+    // 在项目的js文件中, 只要有 import Vue from 'vue'; 的导入
+    // 都会将导入的 vue 指向这里配置的问价
+    alias: {
+      // runtime-only中不允许有任何的template代码
+      // 将vue文件指向指定的vue发布版本
+      // vue/dist/vue.esm.js版本中包括了需要的runtime-compiler
+      // 如果这里不进行配置指定, 他会默认去使用vue/dist/vue.runtime.js
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
 }
