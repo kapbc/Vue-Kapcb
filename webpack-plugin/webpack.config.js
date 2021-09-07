9e90b4cc350b2e34af9e8ce4a38543e5d797d708
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-07 22:07:54 +0800

    UglifyJsPlugin

--- webpack-plugin/webpack.config.js
+++ webpack-plugin/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 // 添加版权的plugin
 const webpack = require('webpack');
+// 添加vue解析的plugin
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+// 添加HTML打包plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 添加js压缩插件
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -30,13 +34,17 @@ module.exports = {
     extensions: ['.vue']
   },
   plugins: [
+    // 解析vue插件
     new VueLoaderPlugin(),
+    // 添加banner版权信息插件
     new webpack.BannerPlugin({
       banner: 'Author By Kpacb'
     }),
+    // 生成HTML插件
     new HtmlWebpackPlugin({
       // 指定模板, 就会按照webpack.config.js所在的同级目录下的index.html, 依此为模板进行生城
       template: 'index.html'
     }),
+    new UglifyJsPlugin(),
   ]
 }
