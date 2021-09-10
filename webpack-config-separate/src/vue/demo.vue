e7a2ce4da07b98ce002ca3edd1f3be52ed4639dc
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-10 21:53:02 +0800

    config

---
+++ webpack-config-separate/src/vue/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3 class="title">demo中的组件</h3>
+    <button @click="btnClick">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo",
+  data() {
+    return {
+      message: 'Hello Vue!',
+    }
+  },
+  methods: {
+    btnClick() {
+      alert(this.message);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: 18px;
+  color: orange;
+}
+</style>
--- webpack-config-separate/webpack.config.js
+++ webpack-config-separate/webpack.config.js
@@ -1,1 +1,49 @@
 const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const UglifyjsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: ['vue-loader']
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      crypto: false
+    },
+    extensions: ['.vue', '.ts', '.js', '.sass', '.css']
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.BannerPlugin({
+      banner: 'Author By Kapcb'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new UglifyjsPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    inline: true,
+    port: 3000,
+    open: true
+  }
+}
