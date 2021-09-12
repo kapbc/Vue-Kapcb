cf595195b67ee3718b218cc94faf702cd099e07d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-12 21:45:34 +0800

    render

--- vue-2-test-runtime-compiler/src/main.js
+++ vue-2-test-runtime-compiler/src/main.js
@@ -6,12 +6,45 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+const demo = {
+  template: `
+    <div>
+    <h3>{{ message }}</h3>
+    <button @click="btnClick">按钮</button>
+    </div>`,
+  data () {
+    return {
+      message: '传入组件到render函数',
+      clickMessage: '这是组件中的点击事件'
+    }
+  },
+  methods: {
+    btnClick () {
+      alert(this.clickMessage)
+    }
+  }
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  // components: { App },
+  // template: '<App/>'
+  render: function (createElement) {
+    // 1.普通用法:
+    // 使用render函数创建DOM元素
+    // 参数: createElement('标签(字符串)', {标签的属性}(对象), ['标签对中的内容'](数组))
+    // return createElement(
+    //   'h2',
+    //   { class: 'box' },
+    //   ['Hello Vue! This is render function create element! I am Kapcb!', createElement(
+    //     'button',
+    //     ['按钮'])])
+
+    // 2.传入组件对象:
+    return createElement(demo)
+  }
 })
 
 // runtime-compiler
