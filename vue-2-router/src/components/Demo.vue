197faf3fd87cc504c5cbe2702772184292be249c
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-14 22:48:51 +0800

    router

---
+++ vue-2-router/src/components/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3 class="title">{{ title }}</h3>
+    <button @click="alert">按钮</button>
+    <router-link to="/">返回上一页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data () {
+    return {
+      title: 'Hello Vue!',
+      alertMessage: 'Hello Kapcb!'
+    }
+  },
+  methods: {
+    alert () {
+      alert(this.alertMessage)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: 22px;
+  color: orange;
+}
+</style>
--- vue-2-router/src/router/index.js
+++ vue-2-router/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Demo from '@/components/Demo'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/demo',
+      name: 'Demo',
+      component: Demo
     }
   ]
 })
