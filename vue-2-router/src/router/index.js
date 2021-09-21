5f4c72ab30346a18f2bc7dc77704587f3ba74807
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-21 11:46:11 +0800

    router

--- vue-2-router/src/router/index.js
+++ vue-2-router/src/router/index.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
+// 配置导入路由相关的信息
 import Router from 'vue-router'
 import Demo from '@/components/Demo'
 import HelloWorld from '@/components/HelloWorld'
 
+// 1.通过Vue.use(插件) : 安装插件(通过vue的方式安装插件)
 Vue.use(Router)
 
+// 配置路由组件映射关系
+const routers = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/demo',
+    name: 'Demo',
+    component: Demo
+  }
+]
+
+// 2.创建VueRouter路由对象
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/demo',
-      name: 'Demo',
-      component: Demo
-    }
-  ]
+  // 配置路径和组件之间的映射关系
+  routes: routers
 })
