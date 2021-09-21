6c8bdd138ddf91a1199bfe1638838d924fbff095
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-21 15:43:56 +0800

    router

--- vue-2-router/src/App.vue
+++ vue-2-router/src/App.vue
@@ -21,4 +21,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.router-link-active {
+  color: orange;
+}
 </style>
--- vue-2-router/src/components/index.vue
+++ vue-2-router/src/components/index.vue
@@ -1,10 +1,11 @@
 <template>
-<div>
-  <h2>index页面</h2>
-  <h4>默认访问首页</h4>
-  <router-link to="/helloWorld">访问HelloWords页面</router-link>
-  <router-link to="/demo">访问demo页面</router-link>
-</div>
+  <div>
+    <h2>index页面</h2>
+    <h4>默认访问首页</h4>
+    <!-- tag属性可以指定让router-link渲染之后的元素 -->
+    <router-link tag="button" to="/helloWorld">访问HelloWords页面</router-link>
+    <router-link tag="div" to="/demo" replace>访问demo页面</router-link>
+  </div>
 </template>
 
 <script>
