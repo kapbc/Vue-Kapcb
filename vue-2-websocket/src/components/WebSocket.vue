6f7e234a808c56c31dfd875ec91e4087f4a6df4f
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-23 20:34:25 +0800

    websocket

---
+++ vue-2-websocket/src/components/WebSocket.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Web Socket Page</h3>
+    <input type="text" placeholder="pls type your message" v-model="message">
+    <button @click="sendMessage">发送</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebSocket',
+  data () {
+    return {
+      message: '',
+
+    }
+  },
+  methods: {
+    sendMessage () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-2-websocket/src/router/index.js
+++ vue-2-websocket/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import WebSocket from '@/components/WebSocket'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/socket',
+    name: 'WebSocket',
+    component: WebSocket
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes,
+  mode: 'history',
+  linkActiveClass: 'active'
 })
